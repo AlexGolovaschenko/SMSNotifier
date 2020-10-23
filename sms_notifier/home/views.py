6120d91b1b602eeb593aa5262b4fbbc28550a495
Aleksandr <golovasanya@mail.ru>
2020-10-23 16:14:09 +0300

    displayed sms server connection settings

--- sms_notifier/home/views.py
+++ sms_notifier/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from owencloud_connector.models import CloudConnector, CloudEvent 
+from notifier.models import Notifier
 
 def home(request):
 	return render(request, 'home/home.html')
@@ -12,9 +13,11 @@ def settings(request):
 	user = request.user
 	connector = CloudConnector.objects.get(user=user)
 	cloud_events = CloudEvent.objects.filter(cloud_connector = connector)
+	sms_notifier = Notifier.objects.get(user=user)
 
 	context = {
 		'connector': connector,
-		'cloud_events': cloud_events
+		'cloud_events': cloud_events,
+		'sms_notifier': sms_notifier,
 	}
 	return render(request, 'home/settings.html', context)
--- sms_notifier/notifier/models.py
+++ sms_notifier/notifier/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.html import mark_safe
 
 from datetime import time
 
@@ -8,14 +8,17 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 from owencloud_connector.models import CloudEvent
 
+
+SMS_RU = 'SMS.RU'
 SMS_SERVICES = {
-	('SMS.RU', 'SMS.RU'),
+	(SMS_RU, 'SMS.RU'),
 }
 
 
 class Notifier(models.Model):
 	user = models.ForeignKey(User, on_delete = models.CASCADE)
 	sms_service = models.CharField(max_length=20, blank=False, null=False, choices=SMS_SERVICES, verbose_name='Сервис рассылки СМС')
+	sms_service_token = models.CharField(max_length=200, blank=False, null=False, verbose_name='Токен подключения к СМС сервису')
 
 	def __str__(self):
 		return str(self.user.username) + ' уведомления'
@@ -24,6 +27,10 @@ class Notifier(models.Model):
 		verbose_name = 'Уведомление'
 		verbose_name_plural = 'Уведомления'
 
+	@property
+	def sms_service_link(self):
+		if self.sms_service == SMS_RU:
+			return mark_safe('<a href="https://sms.ru/"> %s <a/>' %(SMS_RU))
 
 
 class Event(models.Model):
